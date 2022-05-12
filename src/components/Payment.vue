580396c8d1bc78fda8baac07946e1df0748b127a
Yuki Hosokawa <y.hosokawa.r@gmail.com>
2022-05-12 13:53:54 +0900

    作成したコンポーネントを適応する

---
+++ src/components/Payment.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div class="container">
+        <label>Payment.js</label>
+        <h1>最近の支出</h1>
+        <div class="payment"> 
+            <label>カレーライス</label>
+            <label>400円</label>
+        </div>
+
+    </div>
+</template>
+
+<style scoped>
+.container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+.payment {
+    display: flex;
+    justify-content: space-between;
+    height: 80px;
+    width: 400px;
+    align-items: center;
+    background-color: aliceblue;
+}
+
+label {
+    font-size: 20px;
+    font-weight: bold;
+}
+</style>
