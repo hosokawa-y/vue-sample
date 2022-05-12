254b451b39ce430d049f3a672dcc9373a86e50ec
Yuki Hosokawa <y.hosokawa.r@gmail.com>
2022-05-12 14:03:39 +0900

    v-bindで変数を埋め込む

--- src/components/Payment.vue
+++ src/components/Payment.vue
@@ -1,13 +1,27 @@
 <script setup lang="ts">
+ const itemName1 = 'Desk'
+ const itemName2 = 'bike'
+ const price1 = 400
+ const price2 = 800
+ const url1 = "https://www.amazon.co.jp/【国内正規品-メーカー保証1年】FELLOW-Stagg-温度調整機能付き電気ケトル-【PSE認証済】/dp/B09MKFCKYY"
 </script>
 
 <template>
     <div class="container">
         <label>Payment.js</label>
         <h1>最近の支出</h1>
-        <div class="payment"> 
-            <label>カレーライス</label>
-            <label>400円</label>
+        <div class="payment">
+            <label>{{ itemName1 }}</label>
+            <label>{{price1}}円</label>
+            <a v-bind:href="url1">bought at...</a>
+        </div>
+        <div class="payment">
+            <label>{{ itemName2}}</label>
+            <label>{{price2}}円</label>
+        </div>
+        <div class="payment">
+            <label>そば</label>
+            <label>800円</label>
         </div>
 
     </div>
@@ -27,6 +41,7 @@
     width: 400px;
     align-items: center;
     background-color: aliceblue;
+    margin-bottom: 8px;
 }
 
 label {
